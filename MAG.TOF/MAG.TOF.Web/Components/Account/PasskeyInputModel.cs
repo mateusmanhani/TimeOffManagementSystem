8367bbcbeaf981be0f8767b76153e574ba95f6af
João Ribeiro Neto <yhohanes@hotmail.com>
2025-12-29 16:17:19 +0000

    Initial commit

---
+++ MAG.TOF/MAG.TOF.Web/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace MAG.TOF.Web.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
